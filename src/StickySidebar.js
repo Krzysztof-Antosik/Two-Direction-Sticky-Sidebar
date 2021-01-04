907a6189e5fb93a4a23ec986847ae1100059e61f
Antosik-dev <krzysztof@antosik.dev>
2021-01-04 17:53:46 +0100

    Fixed a bug when the sidebar was smaller than the screen

--- src/StickySidebar.js
+++ src/StickySidebar.js
@@ -4,25 +4,36 @@ const aside = document.querySelector('[data-sticky="true"]'),
 startScroll = 0;
 var endScroll = window.innerHeight - aside.offsetHeight -500,
 currPos = window.scrollY;
+screenHeight = window.innerHeight,
+asideHeight = aside.offsetHeight;
 aside.style.top = startScroll + 'px';
+//check height screen and aside on resize
+window.addEventListener('resize', ()=>{
+    screenHeight = window.innerHeight;
+    asideHeight = aside.offsetHeight;
+});
 //main function
 document.addEventListener('scroll', () => {
     endScroll = window.innerHeight - aside.offsetHeight;
     let asideTop = parseInt(aside.style.top.replace('px;', ''));
-    if (window.scrollY < currPos) {
-        //scroll up
-        if (asideTop < startScroll) {
-            aside.style.top = (asideTop + currPos - window.scrollY) + 'px';
-        } else if (asideTop >= startScroll && asideTop != startScroll) {
-            aside.style.top = startScroll + 'px';
+    if(asideHeight>screenHeight){
+        if (window.scrollY < currPos) {
+            //scroll up
+            if (asideTop < startScroll) {
+                aside.style.top = (asideTop + currPos - window.scrollY) + 'px';
+            } else if (asideTop >= startScroll && asideTop != startScroll) {
+                aside.style.top = startScroll + 'px';
+            }
+        } else {
+            //scroll down
+            if (asideTop > endScroll) {
+                aside.style.top = (asideTop + currPos - window.scrollY) + 'px';
+            } else if (asideTop < (endScroll) && asideTop != endScroll) {
+                aside.style.top = endScroll + 'px';
+            }
         }
-    } else {
-        //scroll down
-        if (asideTop > endScroll) {
-            aside.style.top = (asideTop + currPos - window.scrollY) + 'px';
-        } else if (asideTop < (endScroll) && asideTop != endScroll) {
-            aside.style.top = endScroll + 'px';
-        }
+    }else{
+        aside.removeAttribute('style');
     }
     currPos = window.scrollY;
 }, {
