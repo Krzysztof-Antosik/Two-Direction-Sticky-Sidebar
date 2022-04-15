2c0aad5e9fbf2bb993afcbd0f33e0783b5711de4
Krzysztof-Antosik <krzysztof@antosik.dev>
2022-04-15 02:44:45 +0200

    Fixed errors, the code has been optimized, now you do not need to add additional CSS code, GAPS functioning now works correctly, and their attributes are not required to operate the code, zaktualizowano przykład oraz plik README

--- src/StickySidebar.js
+++ src/StickySidebar.js
@@ -1,51 +1,61 @@
-//aside selector
-const aside = document.querySelector('[data-sticky="true"]'),
+//Sticky element selector..
+const stickyElement = document.querySelector('[data-sticky="true"]'),
 
-//varibles
-startScroll = parseInt(aside.getAttribute('data-top-gap')),
-bottomGap = parseInt(aside.getAttribute('data-bottom-gap'));
-var endScroll = window.innerHeight - aside.offsetHeight - 500,
+//..and other constant
+startPosition = stickyElement.getBoundingClientRect().top,
+topGap = stickyElement.hasAttribute('data-top-gap') ? parseInt(stickyElement.getAttribute('data-top-gap')) : 0,
+bottomGap =  stickyElement.hasAttribute('data-bottom-gap') ? parseInt(stickyElement.getAttribute('data-bottom-gap')) : 0;
+
+//Varibles
+var endScroll = window.innerHeight - stickyElement.offsetHeight - 500,
 currPos = window.scrollY,
 screenHeight = window.innerHeight,
-asideHeight = aside.offsetHeight;
-aside.style.top = startScroll + 'px';
+stickyElementHeight = stickyElement.offsetHeight;
+stickyElement.style.top = topGap + 'px';
 
-//check heights of the viewport and the aside on window resize and reapply positioning
+//Add required style to sticky element
+stickyElement.style.marginTop = topGap + 'px';
+stickyElement.style.marginBottom = bottomGap + 'px';
+stickyElement.style.position = 'sticky';
+stickyElement.style.top = topGap + 'px'; 
+
+//Main function
+function position_sticky_sidebar() {
+    endScroll = window.innerHeight - stickyElement.offsetHeight - bottomGap;
+    let stickyElementTop = parseInt(stickyElement.style.top.replace('px;', ''));
+    if (stickyElementHeight>screenHeight) {
+        if (window.scrollY < currPos) {
+
+            //Scroll up
+            if (stickyElementTop < topGap) {
+                stickyElement.style.top = (stickyElementTop + currPos - window.scrollY) + 'px';
+            } else if (stickyElementTop >= topGap && stickyElementTop != topGap) {
+                stickyElement.style.top = topGap + 'px';
+            }
+        } else {
+
+            //Scroll down
+            if (stickyElementTop > endScroll) {
+                stickyElement.style.top = (stickyElementTop + currPos - window.scrollY) + 'px';
+            } else if (stickyElementTop < (endScroll) && stickyElementTop != endScroll) {
+                stickyElement.style.top = endScroll + 'px';
+            }
+        }
+    } else {
+        stickyElement.style.top = topGap + 'px';
+    }
+    currPos = window.scrollY;
+}
+
+//Check heights of the viewport and the sticky element on window resize and reapply positioning
 window.addEventListener('resize', ()=>{
     screenHeight = window.innerHeight;
-    asideHeight = aside.offsetHeight;
+    stickyElementHeight = stickyElement.offsetHeight;
     position_sticky_sidebar();
 });
 
+//Follow the main function when scrolling
 document.addEventListener('scroll', position_sticky_sidebar, {
     capture: true,
     passive: true
 });
-
-//main function
-function position_sticky_sidebar() {
-    endScroll = window.innerHeight - aside.offsetHeight - bottomGap;
-    let asideTop = parseInt(aside.style.top.replace('px;', ''));
-    if (asideHeight>screenHeight) {
-        if (window.scrollY < currPos) {
-
-            //scroll up
-            if (asideTop < startScroll) {
-                aside.style.top = (asideTop + currPos - window.scrollY) + 'px';
-            } else if (asideTop >= startScroll && asideTop != startScroll) {
-                aside.style.top = startScroll + 'px';
-            }
-        } else {
-
-            //scroll down
-            if (asideTop > endScroll) {
-                aside.style.top = (asideTop + currPos - window.scrollY) + 'px';
-            } else if (asideTop < (endScroll) && asideTop != endScroll) {
-                aside.style.top = endScroll + 'px';
-            }
-        }
-    } else {
-        aside.style.top = startScroll + 'px';
-    }
-    currPos = window.scrollY;
-}
